a807c76edf7551950b80b0664a799cad25bfcc60
Evening <johnchangqi@hotmail.com>
2023-09-27 13:28:21 +0800

    Update basename

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="FRDC-UI">
       <Routes>
         <Route path="/" element={<Login />}/>
         <Route path="/voting" element={<VotingPage/>}/>
